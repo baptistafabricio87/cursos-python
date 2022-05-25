f6edaa92936598b8fd0a6f5d0b41f58d6557abc3
Fabricio Castro <baptistafabricio87@gmail.com>
2022-05-25 11:37:39 -0300

    adicionado verificação de quantidade de letras

--- jogos/forca.py
+++ jogos/forca.py
@@ -2,20 +2,33 @@
 
 def jogar():
     palavra_secreta = 'banana'
+    letras_certas = ['_', '_', '_', '_', '_', '_']
+    letras_faltando = str(letras_certas.count('_'))
     enforcou = False
     acertou = False
 
-    while (not enforcou and not acertou):
+    print(letras_certas)
+    print(f'Ainda falta {letras_faltando} letras.')
+    while not enforcou and not acertou:
         chute = input('Qual letra ?  ')
         chute = chute.strip()
-        index = 1
+        index = 0
         for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                print('Letra {} na posicao {}'.format(letra, index))
+            if chute.upper() == letra.upper():
+                letras_certas[index] = letra
             index += 1
 
-    print('Fim do jogo')
+        print(letras_certas)
 
+        letras_faltando = str(letras_certas.count('_'))
+        if letras_faltando == '0':
+            acertou = True
+            print('Parabens! Se salvou!!!')
+        elif len(letras_faltando) < len(letras_certas):
+            print(f'Ainda falta {letras_faltando} letras.')
+        else:
+            enforcou = True
+            print('Enforcou! :p')
 
 if __name__ == '__main__':
     jogar()
