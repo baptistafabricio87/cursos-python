2ad09c023f4edc8fdcd7a8c1682799c5076f640b
Fabricio Castro <baptistafabricio87@gmail.com>
2022-05-26 10:51:15 -0300

    Criando primeira classe.

---
+++ oo/README.md
@@ -0,0 +1,2 @@
+# Projeto para estudo do paradigma Orientado a Objetos com Python3
+
---
+++ oo/conta.py
@@ -0,0 +1,12 @@
+
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite = 1000.0):
+        print("Contruindo objeto ... {}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
+    
