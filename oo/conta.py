ba64fa839e54c69ec2107d3085fdf994926fe5e0
Fabricio Castro <baptistafabricio87@gmail.com>
2022-05-27 15:42:39 -0300

    ADD modulo e classe Conta

--- oo/conta.py
+++ oo/conta.py
@@ -9,4 +9,18 @@ class Conta:
         self.saldo = saldo
         self.limite = limite
 
-    
+    def extrato(self):
+        print("Saldo: {} - Titular: {} ".format(self.saldo, self.titular))
+
+    def cria_conta(numero, titular, saldo, limite):
+        conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
+        return conta
+
+
+    def deposito(self, valor):
+        self.saldo += valor
+
+
+    def saca(self, valor):
+        self.saldo -= valor
+
