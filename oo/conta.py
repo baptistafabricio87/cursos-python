93203d85d65ac37755e023e7590b338ad8e3ac38
Fabricio Castro <baptistafabricio87@gmail.com>
2022-05-30 21:35:28 -0300

    Conclusão do primeiro modulo de OO

--- oo/conta.py
+++ oo/conta.py
@@ -2,7 +2,7 @@
 
 class Conta:
 
-    def __init__(self, numero, titular, saldo, limite = 1000.0):
+    def __init__(self, numero, titular, saldo, limite=1000.0):
         print("Objeto construido ... {}".format(self))
         self.__numero = numero
         self.__titular = titular
@@ -12,19 +12,27 @@ class Conta:
     def extrato(self):
         print("\nSaldo: {} - Titular: {} ".format(self.__saldo, self.__titular))
 
-
     def deposito(self, valor):
         self.__saldo += valor
 
+    def __valida_saldo(self, valor_do_saque):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor_do_saque <= valor_disponivel
 
     def saca(self, valor):
-        self.__saldo -= valor
+        if self.__valida_saldo(valor):
+            self.__saldo -= valor
+            print("Saque efetuado no valor:", valor)
+        else:
+            print("O valor {} passou o limite".format(valor))
 
-
-    def transfere(self, c_destino, valor ):
-        self.saca(valor)
-        c_destino.deposito(valor)
-    
+    def transfere(self, c_destino, valor):
+        if self.__valida_saldo():
+            self.saca(valor)
+            c_destino.deposito(valor)
+            print("transferencia efetuada!")
+        else:
+            print("O valor {} passou o limite".format(valor))
 
     def get_numero(self):
         return self.__numero
@@ -36,7 +44,7 @@ class Conta:
     @property
     def saldo(self):
         return self.__saldo
-    
+
     @property
     def limite(self):
         return self.__limite
@@ -44,3 +52,13 @@ class Conta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
--- oo/teste.py
+++ oo/teste.py
@@ -6,36 +6,42 @@ from datas import Data
 conta1 = Conta(1, "Fabricio", 500.0)
 conta2 = Conta(2, "Tatiana", 400.0)
 
-# EXERCICIO - PROPRIEDADES
-print("\nMostra limite: ", conta1.limite)
-x = float(input("Informe novo limite: "))
-conta1.limite = x
-print("Novo limite é: ", conta1.limite)
+conta1.saca(1500.0)
 
-# ENCAPSULAMENTO
-conta1.transfere(conta2, 50.0)
-conta2.extrato()
+# # EXEMPLO METODO ESTATICO
+# print(Conta.codigo_banco())
 
-# METODOS
-print("Extrato ")
-conta1.extrato()
 
-print("\nSaque ")
-conta1.saca(40.0)
-print("Extrato ")
-conta1.extrato()
+# # EXERCICIO - PROPRIEDADES
+# print("\nMostra limite: ", conta1.limite)
+# x = float(input("Informe novo limite: "))
+# conta1.limite = x
+# print("Novo limite é: ", conta1.limite)
 
-print("\nDeposito")
-conta1.deposito(50.0)
-print("Extrato ")
-conta1.extrato()
+# # ENCAPSULAMENTO
+# conta1.transfere(conta2, 50.0)
+# conta2.extrato()
 
-# EXERCICIO 1
-pessoa1 = Pessoa("Fabricio", "Castro")
-print("\nObjeto Pessoa")
-pessoa1.exibe_nome()
+# # METODOS
+# print("Extrato ")
+# conta1.extrato()
 
-# EXERCICIO 2
-print("\nExercicio DATAS")
-d = Data(27, 5, 2022)
-d.dt_formatada()
+# print("\nSaque ")
+# conta1.saca(40.0)
+# print("Extrato ")
+# conta1.extrato()
+
+# print("\nDeposito")
+# conta1.deposito(50.0)
+# print("Extrato ")
+# conta1.extrato()
+
+# # EXERCICIO 1
+# pessoa1 = Pessoa("Fabricio", "Castro")
+# print("\nObjeto Pessoa")
+# pessoa1.exibe_nome()
+
+# # EXERCICIO 2
+# print("\nExercicio DATAS")
+# d = Data(27, 5, 2022)
+# d.dt_formatada()
