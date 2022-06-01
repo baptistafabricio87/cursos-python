c4b233ff8dd538c35af9d213a60cff9f73839841
Fabricio Castro <baptistafabricio87@gmail.com>
2022-06-01 15:35:17 -0300

    Conceitos - plomorfismo e sobrescrita de metodo

--- oo_2/modelo.py
+++ oo_2/modelo.py
@@ -1,47 +1,55 @@
 class Programa:
 
     def __init__(self, nome, ano):
-        self.__nome = nome.title()
-        self.__ano = ano
-        self.__likes = 0
+        self._nome = nome.title()
+        self._ano = ano
+        self._likes = 0
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
 
     @nome.setter
     def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+        self._nome = novo_nome.title()
 
     @property
     def ano(self):
-        return self.__ano
+        return self._ano
 
     @property
     def likes(self):
-        return self.__likes
+        return self._likes
 
     def dar_likes(self):
-        self.__likes += 1
+        self._likes += 1
+
+    def imprime(self):
+        print(f'{self.nome} - {self.ano} - {self.likes} Likes')
 
 
 class Filme(Programa):
 
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
-        self.__duracao = duracao
+        self._duracao = duracao
 
     @property
     def duracao(self):
-        return self.__duracao
+        return self._duracao
 
+    def imprime(self):
+        print(f'{self.nome} - {self.ano} - {self.duracao} min - {self.likes} Likes')
 
 class Serie(Programa):
 
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
-        self.__temporadas = temporadas
+        self._temporadas = temporadas
 
     @property
     def temporadas(self):
-        return self.__temporadas
+        return self._temporadas
+
+    def imprime(self):
+        print(f'{self.nome} - {self.ano} - {self.temporadas} temp - {self.likes} Likes')
--- oo_2/teste.py
+++ oo_2/teste.py
@@ -1,14 +1,22 @@
 from modelo import Filme, Serie
 
-filme = Filme('vingadores - guerra givil', 2018, 160)
-filme.dar_likes()
-print(f'Nome: {filme.nome} - Ano: {filme.ano} - Duracao: {filme.duracao} - Likes: {filme.likes}')
+filme = Filme('vingadores - guerra inifita', 2018, 160)
+serie = Serie('atlanta - de glover', 2018, 2)
 
-serie = Serie('atlanta', 2018, 2)
-serie.dar_likes()
-print(f'Nome: {serie.nome} - Ano: {serie.ano} - Temporadas: {serie.temporadas} - Likes: {serie.likes}')
+filmes_series = [filme, serie]
 
-atlanta = Serie('atlanta', 2018, 2)
-atlanta.nome = 'atlanta - de glover'
-atlanta.dar_likes()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+for programa in filmes_series:
+    programa.imprime()
+
+
+
+
+
+# filme.dar_likes()
+# print(f'Nome: {filme.nome} - Likes: {filme.likes}')
+
+# serie.dar_likes()
+# print(f'Nome: {serie.nome} - Likes: {serie.likes}')
+
+# atlanta.dar_likes()
+# print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
